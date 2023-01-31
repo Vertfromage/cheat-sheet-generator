60f1344bef271e8680ae104af4f68b51a3ba9ae2
Crystal Parker <vertfromage@gmail.com>
2023-01-31 18:53:27 -0400

    first commit, finally have a workable idea, it's going to be an app where you can search for mathjax formulas by name, category, course and drag the visual version of them onto a printable cheatsheet. It's also going to have login, hopefully.

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom' 
+import React from "react";
+import { MathJaxContext } from "better-react-mathjax"
+
+
+
+// pages and components
+import Home from './pages/Home'
+
+function App() {
+  // config for MathJaxContext  
+  const config = {
+    loader: { load: ["[tex]/html"] },
+    tex: {
+      packages: { "[+]": ["html"] },
+      inlineMath: [
+        ["$", "$"],
+        ["\\(", "\\)"]
+      ],
+      displayMath: [
+        ["$$", "$$"],
+        ["\\[", "\\]"]
+      ]
+    }
+  }
+
+  
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <div className='pages'>
+        <Routes>
+          <Route
+          path ='/'
+          element = {<MathJaxContext config={config}><Home/></MathJaxContext>}
+          />
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,32 @@
+import { MathJax } from "better-react-mathjax"
+
+const Home = () => {
+    // temporary testing
+    let test1 =  {
+            name : "inline",
+            formula : String.raw`$\sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6}$`
+        }
+    let test2 = {
+            name : "display",
+            formula : String.raw`$$\sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6}$$`
+        }
+    let test3 =   {
+            name : "Invisible paranthesis",
+            formula : String.raw`$$\left.x^2\right\rvert_3^5 = 5^2-3^2$$`
+        }
+
+    return (
+        <MathJax>
+        <div className="home">
+            <h2>Home</h2>
+            <h3>Test 1</h3>
+            <p>{test1.name +" "+ test1.formula}</p>
+            <h3>{test2.name + test2.formula}</h3>
+            <h3>{test3.name + test3.formula}</h3>
+            
+        </div>
+        </MathJax>
+    )
+}
+
+export default Home
