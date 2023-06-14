60059722c2d9d6a682d62e672b9cad9bec047c46
Crystal Parker <vertfromage@gmail.com>
2023-06-14 13:24:18 -0300

    Getting set up for more local development, commented out AWS SNS code because it's not important and I don't want to connect to AWS right now.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+
+// Database
 const dynamoose = require('dynamoose')
+
+// Routes Files
 const formulaRoutes = require('./routes/FormulaRoutes')
 const userRoutes = require('./routes/UserRoutes')
 const pageRoutes = require('./routes/PageRoutes')
@@ -23,18 +27,18 @@ app.use('/api/users', userRoutes)
 app.use('/api/pages', pageRoutes)
 app.use('/api/auth', authRoutes) 
 
-// Create new DynamoDB instance 
-const ddb = new dynamoose.aws.ddb.DynamoDB({
-    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
-    "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
-    "region": process.env.AWS_REGION,
-    "sessionToken": process.env.AWS_SESSION_TOKEN
-});
-//Set DynamoDB instance to the Dynamoose DDB instance
-dynamoose.aws.ddb.set(ddb);
+// // Create new DynamoDB instance 
+// const ddb = new dynamoose.aws.ddb.DynamoDB({
+//     "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+//     "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+//     "region": process.env.AWS_REGION,
+//     "sessionToken": process.env.AWS_SESSION_TOKEN
+// });
+// //Set DynamoDB instance to the Dynamoose DDB instance
+// dynamoose.aws.ddb.set(ddb);
 
 // // Local testing
-// dynamoose.aws.ddb.local()
+dynamoose.aws.ddb.local()
 
 // listen for requests
 app.listen(process.env.PORT, ()=>{
